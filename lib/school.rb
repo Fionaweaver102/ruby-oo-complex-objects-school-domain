7ca48e42205aa2c7e5fc72e7bfb4039705a6c555
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-16 09:45:02 -0700

    commit tree de0efb85a0f14faa44627fbda0d2bfad9ecf79a4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School  
+attr_accessor :roster
+attr_reader :student_name
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new{|r,n| r[n] = []} 
+    end 
+
+    def add_student(student_name, grade)
+        @roster[grade] << student_name
+    end 
+
+    def grade(grade)
+       self.roster[grade]
+    end
+
+    def sort
+        self.roster.each do |grade, students|
+            students.sort!
+        end 
+    end 
+end 
+    
